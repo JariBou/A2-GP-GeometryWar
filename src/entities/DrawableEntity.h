365e7d1a16ca4980e9e4c51f8f57c8650e45f25d
Shahine <shahine.benthanane@edu.devinci.fr>
2023-11-27 18:05:00 +0100

    Debut de drawableentities

--- src/entities/DrawableEntity.h
+++ src/entities/DrawableEntity.h
@@ -11,9 +11,10 @@ namespace Entities
 	public:
 
 		sf::Shape& shape;
+		ShapeType shapeType = 0;
 
 		DrawableEntity(sf::Shape& shape) : shape(shape) {
-
+		
 		}
 
 		virtual ~DrawableEntity() = default;
@@ -21,6 +22,42 @@ namespace Entities
 		virtual void Draw(sf::RenderWindow& window) {
 			window.draw(this->shape);
 		}
+
+		virtual void Move(sf::Vector2f vectorMove){
+			sf::Vector2f pos = shape.getPosition();
+			pos += vectorMove;
+			shape.setPosition(pos);
+		}
+
+		virtual void SetPosition(sf::Vector2f pos) {
+			shape.setPosition(pos);
+		}
+
+		virtual void SetColor(sf::Color color) {
+			switch (shapeType)
+			{
+				case ShapeType::FILLED:
+					shape.setFillColor(color);
+					shape.setOutlineColor(sf::Color::Transparent);
+					break;
+				case ShapeType::OUTLINED:
+					shape.setOutlineColor(color);
+					shape.setFillColor(sf::Color::Transparent);
+					break;
+				default:
+					break;
+			}
+		}
+
+		virtual void SetShapeType(ShapeType type) {
+			shapeType = type;
+		}
 	};
 
 }
+enum class ShapeType
+{
+	FILLED = 0,
+	OUTLINED = 1,
+};
+
