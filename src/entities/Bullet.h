b26ac4501e6eb5f3609f054cb708aa7590989b0f
Shahine <shahine.benthanane@edu.devinci.fr>
2023-12-01 16:19:54 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/entities/Bullet.h
+++ src/entities/Bullet.h
@@ -8,15 +8,16 @@ namespace Entities {
     class Player; // Assurez-vous que la classe Player est d�clar�e avant Bullet
 
     class Bullet : public DrawableEntity {
-    public:
-        int damage;
-        float speed;
-        Bullet(sf::Shape& shape, Player& player); // Utilisation d'une r�f�rence constante
-        Bullet(sf::Shape& shape, Player& player, int damage, float speed); // Utilisation d'une r�f�rence constante
-        virtual ~Bullet() = default;
-        virtual bool MoveBullet(float deltaTime);
-        virtual void Udapte(float deltaTime);
-        virtual bool CheckLife();
-        Player& player; // Utilisation d'une r�f�rence constante
-    };
+        public:
+            int damage;
+            float speed;
+            Bullet(sf::Shape& shape, Player& player); // Utilisation d'une r�f�rence constante
+            Bullet(sf::Shape& shape, Player& player, int damage, float speed); // Utilisation d'une r�f�rence constante
+            virtual ~Bullet() = default;
+            virtual bool MoveBullet(float deltaTime);
+            virtual void Udapte(float deltaTime);
+            virtual bool CheckLife();
+            Player& player; // Utilisation d'une r�f�rence constante
+       
+};
 }
