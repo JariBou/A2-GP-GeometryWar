d676ce6969ed7501563cb5d1c8651508b8b80df8
Shahine <shahine.benthanane@edu.devinci.fr>
2023-12-04 18:01:03 +0100

    Ajout en cours de NonLinearFoe

---
+++ src/entities/NonLinearFoe.cpp
@@ -0,0 +1,42 @@
+#include "DrawableEntity.h"
+#include "NonLinearFoe.h"
+#include "../UpgradeBoxSpawner.h"
+
+namespace Entities
+{
+	NonLinearFoe::NonLinearFoe(sf::Shape& shape, float speed, Player* player) : Foe(shape, speed, player) {
+	}
+
+	void NonLinearFoe::Update(float deltaTime) {
+		this->Move(direction * speed, deltaTime);
+		sf::Vector2f pos = shape.getPosition();
+		if (pos.y > this->windowHeight + shape.getGlobalBounds().height) {
+			this->health = 0;
+			//Die();
+		}
+	}
+
+	bool NonLinearFoe::isDead() { return this->health <= 0; }
+
+	void NonLinearFoe::SetDirection(sf::Vector2f direction) {
+		this->direction = direction;
+	}
+
+	bool NonLinearFoe::CheckLife() {
+		if (health <= 0) {
+			return false;
+		}
+		return true;
+	}
+
+	void NonLinearFoe::OnKilledByPlayer() {
+		player->GetBoxSpawner()->SpawnUpgradeBox(&shape);
+	}
+
+	void NonLinearFoe::GetHit(float value) {
+		this->health -= value;
+		if (health <= 0) {
+			//Die();
+		}
+	}
+}
---
+++ src/entities/NonLinearFoe.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics.hpp>
+#include "Foe.h"
+#include "Player.h"
+
+namespace Entities
+
+{
+
+	class NonLinearFoe : public Foe {
+
+	public:
+
+		sf::Vector2f direction;
+
+		NonLinearFoe(sf::Shape& shape, float speed, Player* player);
+
+		virtual void Update(float deltaTime);
+		virtual bool isDead();
+		virtual void SetDirection(sf::Vector2f direction);
+		virtual bool CheckLife();
+		virtual void OnKilledByPlayer();
+		virtual void GetHit(float value);
+
+	};
+}
